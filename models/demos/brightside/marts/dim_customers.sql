93a5c9b2b04ef9d513ba0c87a89c2b93f3173bfa
Ollie Clarke <oliver.clarke@theinformationlab.co.uk>
2026-07-15 12:38:53 +0000

    added marts

---
+++ models/demos/brightside/marts/dim_customers.sql
@@ -0,0 +1,13 @@
+select
+    customer_id,
+    first_name || ' ' || last_name as customer_name,
+    email,
+    country,
+    region,
+    case
+        when email ilike '%@brightside-test.co%' then 'Test account'
+        when is_staff                            then 'Staff'
+        else 'Standard'
+    end                            as customer_type,
+    signup_date
+from {{ ref('stg_customers') }}
